4653df427770c12b8a15ecd28af0e119f2ad7ae9
helobrouillette <heloise.brouillette@polymtl.ca>
2020-09-18 14:24:20 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -33,7 +33,7 @@ def main() -> None:
     string = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
+    print(f"Le nombre d'occurrence de l dans hello world est : {get_nb_char(string, 'l')}")
     
     string = "Baby shark doo doo doo doo doo doo"
     print(f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
