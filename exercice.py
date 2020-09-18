7cc2edb54f850aab6981a4d74a288109bb647830
helobrouillette <heloise.brouillette@polymtl.ca>
2020-09-18 14:35:32 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,7 @@ def get_nb_words(sentence: str) -> int:
 
 
 def main() -> None:
-    string = "B"
+    string = ""
     parity = 'pair' if is_even_len(string) else 'impair'
     print(f"Le nombre de caractère dans la chaine '{string}' est {parity}")
 
